1f0c8327489961f698c5ffb2593fecf7a6eaadab
Felipe <fillikz@gmail.com>
2025-05-02 20:32:20 -0300

    feat(core): add IPlayer and ITableProps interfaces for player and table management

---
+++ src/core/interfaces/table-props.ts
@@ -0,0 +1,11 @@
+import { IPlayer } from "./player";
+
+export interface ITableProps {
+    name: string;
+    ownerId: string;
+    token?: string;
+    isVisible?: boolean;
+    isLocked?: boolean;
+    players?: Array<IPlayer>;
+    createdAt?: Date;
+}
