b13427376a140d4ace3eb706bee328c49248c01d
Felipe <fillikz@gmail.com>
2025-05-02 20:56:59 -0300

    fix(IPlayer/ITableProps): format  interfaces for consistency

--- src/core/interfaces/table-props.ts
+++ src/core/interfaces/table-props.ts
@@ -1,11 +1,11 @@
 import { IPlayer } from "./player";
 
 export interface ITableProps {
-    name: string;
-    ownerId: string;
-    token?: string;
-    isVisible?: boolean;
-    isLocked?: boolean;
-    players?: Array<IPlayer>;
-    createdAt?: Date;
+  name: string;
+  ownerId: string;
+  token?: string;
+  isVisible?: boolean;
+  isLocked?: boolean;
+  players?: Array<IPlayer>;
+  createdAt?: Date;
 }
