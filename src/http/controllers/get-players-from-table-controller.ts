092d1a7cd5bd2448e50f596001f7d37aadf4a1ad
Felipe <fillikz@gmail.com>
2025-05-03 13:25:44 -0300

    feat(get-players-from-table-controller): add table existence check and improve error handling

--- src/http/controllers/get-players-from-table-controller.ts
+++ src/http/controllers/get-players-from-table-controller.ts
@@ -1,5 +1,6 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 
+import { singletonTableRepository } from "@/repositories/singleton-table-repository";
 import { makeGetPlayersFromTableUseCase } from "@/use-cases/factories/make-get-players-from-table-use-case";
 
 export const getPlayersFromTableController = async (
@@ -8,13 +9,23 @@ export const getPlayersFromTableController = async (
 ) => {
   const { token } = request.params;
 
+  const table = await singletonTableRepository.findByToken(token);
+
+  if (!table) {
+    return reply.status(404).send({
+      error: "Table not found",
+    });
+  }
+
   const getPlayersFromTableUseCase = makeGetPlayersFromTableUseCase();
 
   try {
     const { players } = await getPlayersFromTableUseCase.execute(token);
 
     return reply.status(200).send({ players });
-  } catch (error) {
-    return reply.status(404).send({ error });
+  } catch {
+    return reply.status(500).send({
+      error: "An error occurred while retrieving players from the table",
+    });
   }
 };
