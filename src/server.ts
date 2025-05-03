8b205aec2eb3e1566c666b9df7a1e33b17eb3ca1
Felipe <fillikz@gmail.com>
2025-05-03 12:10:40 -0300

    feat(use-cases): update tables use-cases folder

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,10 @@ import fastify from "fastify";
 import { tablesRoutes } from "./http/routes/tables";
 
 const app = fastify();
+const port = 3333;
 
 app.register(tablesRoutes);
 
-app.listen({ port: 3333 }).then(() => {
-  console.log("🚀 HTTP server running at http://localhost:3333");
+app.listen({ port }).then(() => {
+  console.log(`"HTTP server is running at http://localhost:${port}`);
 });
