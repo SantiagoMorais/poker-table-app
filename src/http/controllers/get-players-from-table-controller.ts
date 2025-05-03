adf40f27f32c4f6ab6f75130606ac427dc162dba
Felipe <fillikz@gmail.com>
2025-05-03 10:56:08 -0300

    feat(GetPlayersFromTable): configure use case, controller and route

---
+++ src/http/controllers/get-players-from-table-controller.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+
+import { InMemoryTableRepository } from "@/repositories/in-memory/in-memory-tables-repository";
+import { GetPlayersFromTableUseCase } from "@/use-cases/get-players-from-table-use-case";
+
+export const getPlayersFromTableController = async (
+  request: FastifyRequest<{ Params: { token: string } }>,
+  reply: FastifyReply
+) => {
+  const { token } = request.params;
+
+  const tablesRepository = InMemoryTableRepository.getInstance();
+  const getPlayersFromTableUseCase = new GetPlayersFromTableUseCase(
+    tablesRepository
+  );
+
+  try {
+    const { players } = await getPlayersFromTableUseCase.execute(token);
+
+    return reply.status(200).send({ players });
+  } catch (error) {
+    return reply.status(404).send({ error });
+  }
+};
--- src/http/routes/tables.ts
+++ src/http/routes/tables.ts
@@ -2,9 +2,11 @@ import { FastifyInstance } from "fastify";
 
 import { createTableController } from "@/http/controllers/create-table-controller";
 
+import { getPlayersFromTableController } from "../controllers/get-players-from-table-controller";
 import { joinTableController } from "../controllers/join-table-controller";
 
 export const tablesRoutes = async (app: FastifyInstance) => {
   app.post("/tables", createTableController);
   app.post("/tables/join", joinTableController);
+  app.get("/tables/:token/players", getPlayersFromTableController);
 };
