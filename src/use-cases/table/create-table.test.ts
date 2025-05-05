c9b26ee2a8a1f42eef99c598117b07cd1cfc007f
Felipe <fillikz@gmail.com>
2025-05-05 17:50:49 -0300

    refactor(CreateTableUseCase): update constructor to include playersRepository and adjust table creation logic

--- src/use-cases/table/create-table.test.ts
+++ src/use-cases/table/create-table.test.ts
@@ -1,17 +1,21 @@
 import { beforeEach, describe, expect, it } from "vitest";
 
-import { InMemoryTableRepository } from "@/repositories/in-memory/in-memory-tables-repository";
+import { InMemoryPlayersRepository } from "@/repositories/in-memory-repository/in-memory-players-repository";
+import { InMemoryTableRepository } from "@/repositories/in-memory-repository/in-memory-table-repository";
+import { IPlayersRepository } from "@/repositories/players-repository";
 import { ITablesRepository } from "@/repositories/tables-repository";
 
 import { CreateTableUseCase } from "./create-table";
 
-let tablesRepository: ITablesRepository;
+let tableRepository: ITablesRepository;
+let playersRepository: IPlayersRepository;
 let sut: CreateTableUseCase;
 
 describe("CreateTableUseCase", () => {
   beforeEach(() => {
-    tablesRepository = new InMemoryTableRepository();
-    sut = new CreateTableUseCase(tablesRepository);
+    tableRepository = new InMemoryTableRepository();
+    playersRepository = new InMemoryPlayersRepository(tableRepository);
+    sut = new CreateTableUseCase(tableRepository, playersRepository);
   });
 
   it("should be able to create a table", async () => {
--- src/use-cases/table/create-table.ts
+++ src/use-cases/table/create-table.ts
@@ -1,34 +1,35 @@
-import { randomUUID } from "crypto";
-
 import { Table } from "@/core/entities/table";
 import {
   createTableInput,
   TCreateTableInputDTO,
 } from "@/core/types/create-table-input";
+import { IPlayersRepository } from "@/repositories/players-repository";
 import { ITablesRepository } from "@/repositories/tables-repository";
 
 export class CreateTableUseCase {
-  constructor(private tablesRepository: ITablesRepository) {}
+  constructor(
+    private tablesRepository: ITablesRepository,
+    private playersRepository: IPlayersRepository
+  ) {}
 
   async execute(input: TCreateTableInputDTO): Promise<{ table: Table }> {
     const parsed = createTableInput.parse(input);
 
-    const ownerId = randomUUID();
-    const owner = {
-      id: ownerId,
-      name: parsed.ownerName,
-      isOwner: true,
-      chips: 500,
-    };
-
     const table = new Table({
       isLocked: false,
       tableName: parsed.tableName,
-      ownerId: ownerId,
-      players: [owner],
+      players: [],
     });
 
-    await this.tablesRepository.create(table);
+    await this.tablesRepository.create({ table });
+
+    const { player: owner } = await this.playersRepository.create({
+      name: input.ownerName,
+      tableToken: table.token,
+    });
+
+    table.updateOwner({ ownerId: owner.id }, true);
+    await this.tablesRepository.save({ table });
 
     return { table };
   }
