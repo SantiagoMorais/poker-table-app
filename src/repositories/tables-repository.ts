8ff8877127403faa74a64c847769874d2b6c5df5
Felipe <fillikz@gmail.com>
2025-05-05 17:48:47 -0300

    refactor(tables-repository): update method signatures to use destructuring for parameters

--- src/repositories/tables-repository.ts
+++ src/repositories/tables-repository.ts
@@ -1,7 +1,7 @@
 import { Table } from "@/core/entities/table";
 
 export interface ITablesRepository {
-  create(table: Table): Promise<void>;
-  findByToken(token: string): Promise<Table | null>;
-  save(table: Table): Promise<void>;
+  create({ table }: { table: Table }): Promise<void>;
+  findByToken({ token }: { token: string }): Promise<{ table: Table } | null>;
+  save({ table }: { table: Table }): Promise<void>;
 }
